5c26d7c732f375b1db59b9b9f32017a754fbca4b
Rik de Groot <rik.degroot@financiallease.nl>
2023-06-30 09:08:37 +0200

    Add block config

---
+++ i3-term.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# (1) i3 thread: https://faq.i3wm.org/question/150/how-to-launch-a-terminal-from-here/?answer=152#post-id-152
+# (2) http://blog.soulshake.net/2016/03/i3-new-terminals-in-pwd/
+#WHEREAMI=$(cat /tmp/whereami || ~)
+#gnome-terminal --working-directory="$WHEREAMI"
+#hyper
+gnome-terminal
---
+++ lock.sh
@@ -0,0 +1,9 @@
+#/usr/bin/env bash
+
+#xset s off dpms 0 10 0
+
+i3lock \
+    --color=073b77 \
+    --image=/home/rik/.config/i3/Pictures/nyan-cat.png  \
+    --tiling \
+    --nofork
